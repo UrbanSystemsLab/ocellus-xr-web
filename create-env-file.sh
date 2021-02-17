5830440f77191700972cc6be6ad275031f975555
Emily Bowe <emilyrbowe@gmail.com>
2021-02-17 15:00:59 -0500

    Create create-env-file.sh
    Add file that creates .env

---
+++ create-env-file.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Exit the script as soon as something fails.
+set -e
+
+if [ ! -f ./.env ];
+
+then
+  echo "Creating enviornment variables template file. ./.env "
+
+  ENV_VARS='
+  FIREBASE_DEV_KEY=""
+  FIREBASE_DEV_DOMAIN=""
+  FIREBASE_DEV_URL=""
+  FIREBASE_DEV_ID=""
+  FIREBASE_DEV_BUCKET=""
+  FIREBASE_DEV_SENDER_ID=""
+
+  MAPBOX_DEV_TOKEN=""
+
+  '
+  > .env
+
+  echo "${ENV_VARS}" > .env
+
+  echo "Done"
+  echo "Make sure to edit it and provide your own keys."
+
+else
+  echo ".env file already exists. I won't overwrite it."
+fi
