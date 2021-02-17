573508a9545595ef156413b491ab0cd02ad81a59
Emily Bowe <emilyrbowe@gmail.com>
2021-02-17 16:09:53 -0500

    Adding buttons to toggle layers
    Needs CSS fix -- currently being overridden by webkit

--- components/mapView.vue
+++ components/mapView.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+      <nav id="menu"></nav>
       <div id="map"></div>
     </div>
 </template>
@@ -57,8 +58,71 @@ export default {
             //console.log(marker);
           })
         });
-    },
-}
+        map.on('load', function () {
+          map.addSource('tileset-new-york-floodplains--0tb4my', {
+            type: 'vector',
+            url: 'mapbox://equity.dmmqh0kw'
+          });
+          map.addLayer({
+            'id': '500 Year Flood',
+            'type': 'fill',
+            'source': 'tileset-new-york-floodplains--0tb4my',
+            'source-layer': '2020s-500y',
+            'layout': {
+            },
+            'paint': {
+              'fill-color': 'rgb(102, 143, 163)',
+              'fill-opacity': 0.8
+            }
+          });
+          map.addLayer({
+            'id': '100 Year Flood',
+            'type': 'fill',
+            'source': 'tileset-new-york-floodplains--0tb4my',
+            'source-layer': '2020s-100y',
+            'layout': {
+            },
+            'paint': {
+              'fill-color': 'rgb(153, 180, 194)',
+              'fill-opacity': 0.45
+            }
+          });
+        });
+
+        // enumerate ids of the layers
+        var toggleableLayerIds = ['100 Year Flood', '500 Year Flood'];
+
+        // set up the corresponding toggle button for each layer
+        for (var i = 0; i < toggleableLayerIds.length; i++) {
+        var id = toggleableLayerIds[i];
+
+        var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.textContent = id;
+
+        link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        // toggle layer visibility by changing the layout object's visibility property
+        if (visibility === 'visible') {
+        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+        this.className = '';
+        } else {
+        this.className = 'active';
+        map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+        };
+
+        var layers = document.getElementById('menu');
+        layers.appendChild(link);
+        }
+      }
+    }
 </script>
 
 <style scoped>
@@ -66,5 +130,38 @@ export default {
   height: 50vh;
   opacity: 1;
   /* z-index: -100; */
+}
+
+#menu {
+  background: #fff;
+  position: absolute;
+  z-index: 1;
+  top: 450px;
+  right: 10px;
+  border-radius: 3px;
+  width: 120px;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  font-family: 'Open Sans', sans-serif;
+}
+
+#menu a {
+  font-size: 13px !important;
+  color: #404040 !important;
+  display: block !important;
+  margin: 0 !important;
+  padding: 0 !important;
+  padding: 10px !important;
+  text-decoration: none !important;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25) !important;
+  text-align: center !important;
+}
+
+#menu a:last-child {
+border: none !important;
+}
+
+#menu a.active {
+background-color: #3887be !important;
+color: #ffffff !important;
 }
 </style>
