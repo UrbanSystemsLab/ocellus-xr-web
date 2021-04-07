f115c953c8d53ab45aecaafb1d6a9cf8f67e4e2c
Emily Bowe <emilyrbowe@gmail.com>
2021-04-07 15:58:40 -0400

    Cleaned up methods

--- store/index.js
+++ store/index.js
@@ -14,12 +14,6 @@ if (!firebase.apps.length) {
   var storage = firebase.storage()
   var storageRef = storage.ref()
 
-  let mapsTest = db.ref('maps/public');
-  mapsTest.on('value', (snapshot) => {
-      const data = snapshot.val();
-      console.log(data)
-  })
-
 export const state = () => ({
     count: 1,
     allMaps : {},
@@ -34,6 +28,11 @@ export const state = () => ({
     }
 })
 
+export const getters = {
+    getAllMaps: (state) => { return state.allMaps },
+    getCount: (state) => { return state.count }
+}
+
 export const mutations = {
     storeAllMaps: (state, payload) => {
         state.allMaps = Object.assign({}, payload)
@@ -43,62 +42,17 @@ export const mutations = {
 export const actions = {
     getAllMaps: function(state) {
         return new Promise((resolve, reject) => {
-            // var mapsRefPublic = this.$fireModule.database.ref('maps/public')
             var mapsRefPublic = db.ref('maps/public')
-            // var mapsRefPrivate = db.ref('maps/private')
-            // var activeUser = store.getters.getActiveUser
-            // var sources = Object.values(store.getters.getSources)
 
             var publicMaps
-            // var privateMaps
-            mapsRefPublic.once('value')
-            .then(function(snapshot) {
+            mapsRefPublic.on('value', (snapshot) => {
                 publicMaps = Object.assign({}, snapshot.val())
-                for (let map in publicMaps) {
-                    console.log(map)
-                // if (publicMaps.hasOwnProperty(map)) {
-                //     publicMaps[map]['visibility'] = 'public'
-                // }
-                }
-                // // Get private maps
-                // if (activeUser.email && activeUser.emailVerified) return mapsRefPrivate.once('value')
-                // else return false
             })
-            // .then(function(snap) {
-            //     if (snap !== false) {
-            //     privateMaps = Object.assign({}, snap.val())
-            //     for (let map in privateMaps) {
-            //         if (privateMaps.hasOwnProperty(map)) {
-            //         privateMaps[map]['visibility'] = 'private'
-            //         }
-            //     }
-            //     }
-                // // var maps = Object.assign({}, publicMaps, privateMaps)
-                // var maps = Object.assign({}, publicMaps)
-
-                // // aggregate sources into allMaps object
-                // for (var map of Object.values(maps)) {
-                // if (map.layers){
-
-                //     //go through all layers and find matching sources
-                //     let matchingSources = Object.values(map.layers).map(layer => {
-                //     return sources.filter(source => source.id === layer.source)
-                //     }).flat()
-
-                //     // to get only unique values
-                //     map.sources = [... new Set (matchingSources)]
-                // }
-
-                // }
-                state.commit('storeAllMaps', maps)
-                resolve(maps)
-                // console.log(maps)
+            var maps = Object.assign({}, publicMaps)
+            console.log(maps);
+            state.commit('storeAllMaps', maps)
+            resolve(maps)
             })
             .catch((error) => { reject(error) })
         },
-}
-
-export const getters = {
-    getAllMaps: (state) => { return state.allMaps },
-    getCount: (state) => { return state.count }
 }
