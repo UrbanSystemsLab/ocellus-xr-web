c53409a373f7b361a108cdb8fe398eb2995c28bc
Emily Bowe <emilyrbowe@gmail.com>
2021-06-02 08:59:43 -0500

    Created heat impacts page

---
+++ pages/heat-impact.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <main class="flex-container">
+        <appMenu />
+        <el-row>
+            <h2>How am I affected by heat risk?</h2>
+        </el-row>
+        <el-row>
+            <p>Some areas of the city are much hotter in the summer due to how a neighborhood is designed, from heat trapping pavement to a lack of trees, parks or open space.</p>
+        </el-row>
+        </main>
+        <mapView />
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+main {
+    height: 50vh;
+}
+</style>
