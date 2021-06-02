c8882bcd82e92ea7aee05b5c3d46927897122ff5
Emily Bowe <emilyrbowe@gmail.com>
2021-06-02 08:58:42 -0500

    Updated opening screen text

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
     <main class="flex-container">
-      <responseText />
-      <mapView />
+      <appMenu />
+      <el-row>
+        <h2>Clmate Equity XR</h2>
+      </el-row>
+      <el-row>
+          <p>Climate change affects communities in different ways. But how and why? The Climate Equity XR app provides interactive geospatial maps and 3D visualizations of heat, flood risk and other climate risk indicators in New York City. It experiments with augmented reality layers that allow you to project interactive maps onto a physical surface.</p>
+      </el-row>
+      <el-row>
+        <h3>Swipe to see how your community is impacted…</h3>
+      </el-row>
+      <!-- <responseText /> -->
+      <!-- <mapCarousel /> -->
+      <!-- <mapView /> -->
     </main>
 
   </div>
