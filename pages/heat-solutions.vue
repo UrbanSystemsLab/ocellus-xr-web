b45b09ebc6103b296c2f489f5b6c16a1bbc0cfd4
Emily Bowe <emilyrbowe@gmail.com>
2021-06-02 09:00:03 -0500

    Created heat solutions page

---
+++ pages/heat-solutions.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <main class="flex-container">
+        <appMenu />
+        </main>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
