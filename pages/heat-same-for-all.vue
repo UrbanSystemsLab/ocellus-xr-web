d0e67fbc751c4ea24240a79a7d2b56b2f2770740
Emily Bowe <emilyrbowe@gmail.com>
2021-06-02 08:59:56 -0500

    Added heat same for all slide

---
+++ pages/heat-same-for-all.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <main class="flex-container">
+        <appMenu />
+        <el-row>
+            <h2>Are heat impacts the same for all?</h2>
+        </el-row>
+        <el-row>
+            <p>No, low income and BIPOC communities often experience greater climate impacts because people who are more socially vulnerable are more affected by heat.</p>
+        </el-row>
+        </main>
+        <mapCarousel />
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+main {
+    height: 50vh;
+}
+</style>
