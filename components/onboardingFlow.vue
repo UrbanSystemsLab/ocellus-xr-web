48df1d383b47f0481133dd488965655e77fa9dde
Emily Bowe <emilyrbowe@gmail.com>
2021-07-15 18:18:00 -0400

    Added inner dialog for each module

--- components/onboardingFlow.vue
+++ components/onboardingFlow.vue
@@ -1,38 +1,45 @@
 <template>
-    <div id="onboarding-button">
-        <el-button @click="dialogVisible=true" type="info" size="small" id="onboarding" icon="el-icon-info" circle>
-        </el-button>
+    <div>
+        <div id="onboarding-button" :span="12">
+            <el-button @click="dialogVisible=true" type="primary" size="medium" id="onboarding" icon="el-icon-question" circle>
+            </el-button>
+        </div>
         <el-dialog
         :visible.sync="dialogVisible"
-        fullscreen=true
-        modal=true
+        :fullscreen=true
+        :modal=true
         :modalAppendToBody="false">
-        <el-row>
-        <h1>Climate Equity XR</h1>
-        By: <a href="https://urbansystemslab.com/" onclick="window.open('https://urbansystemslab.com', '_system'); return false;">Urban Systems Lab</a>
-        </el-row>
-        <el-row>
-          <p>Climate change affects communities in different ways. But how and why? The Climate Equity XR app provides interactive geospatial maps and 3D visualizations of heat, flood risk and other climate risk indicators in New York City. It experiments with augmented reality layers that allow you to project interactive maps onto a physical surface.</p>
-        </el-row>
-        <el-row>
-            <h2>Am I affected by climate change?</h2>
+            <el-row>
+                <h1>Climate Equity XR</h1>
+                By: <a href="https://urbansystemslab.com/" onclick="window.open('https://urbansystemslab.com', '_system'); return false;">Urban Systems Lab</a>
+            </el-row>
+            <el-row>
+                <p>Climate change affects communities in different ways. But how and why? The Climate Equity XR app provides interactive geospatial maps and 3D visualizations of heat, flood risk and other climate risk indicators in New York City. It experiments with augmented reality layers that allow you to project interactive maps onto a physical surface.</p>
+            </el-row>
+            <el-row>
+                <h3>Am I affected by climate change?</h3>
             </el-row>
             <el-row>
                 <p>Yes! In New York City, we are exposed to multiple climate and weather-related hazards, from heat waves to hurricanes and floods. But some communities are more affected than others. Select below to learn more about how these climate risks affect you.</p>
             </el-row>
-            <el-row>
-                <el-col :span="20">
-                    <el-button type="primary" v-for="(item, itemKey) in onboarding.modules" :key="itemKey">
-                        <h3>{{item.name}}</h3>
-                    </el-button>
-                </el-col>
+            <el-row v-for="(item, itemKey) in onboarding.modules" :key="itemKey" style="display: flex;
+    justify-content: center;">
+                <el-button type="primary" @click="innerVisible=true">
+                    <h3>{{item.name}}</h3>
+                </el-button>
             </el-row>
-        <!-- <el-carousel indicator-position="outside" :autoplay="false">
-            <el-carousel-item v-for="(item, itemKey) in onboarding.modules" :key="itemKey">
-            <div v-html= "item.name"></div>
-            
-            </el-carousel-item>
-        </el-carousel>   -->
+            <el-dialog
+            :visible.sync="innerVisible"
+            :fullscreen=true
+            append-to-body>
+                <el-carousel indicator-position="outside" :autoplay="false">
+                    <el-carousel-item v-for="(item, itemKey) in onboarding.modules" :key="itemKey">
+                    <div v-html= "item.name"></div>
+                    
+                    </el-carousel-item>
+                </el-carousel>  
+            </el-dialog>
+        
         </el-dialog>
     </div>
 </template>
@@ -44,7 +51,8 @@ export default {
     },
     data() {
         return {
-            dialogVisible: false
+            dialogVisible: false,
+            innerVisible: false
         }
     },
     methods: {
@@ -60,19 +68,21 @@ export default {
 </script>
 
 <style>
+div {
+    z-index: 2
+}
 
-div #onboarding-button {
-    z-index: 10;
+#onboarding-button {
+    margin: 15px;
 }
 
 #onboarding {
-    margin-left: 90vw;
-    margin-top: 90vh;
+    top: 90vh;
 }
 
 .el-carousel__container {
     position: relative;
-    height: 40vh;
+    height: 85vh;
     text-align: center;
     margin: auto;
     width: 100%;
@@ -83,9 +93,5 @@ div #onboarding-button {
     border-radius: 10px;
     background-color: #99a9bf;
 }
-
-  /* .el-carousel__arrow--left, .el-carousel__arrow--right {
-      top: 90vh;
-  } */
 
 </style>
