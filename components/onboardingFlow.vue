ebe42b5a2c1a4eef0ad440607a8f5b7f09d1a9f2
Emily Bowe <emilyrbowe@gmail.com>
2021-07-15 13:34:17 -0400

    Getting firebase data into carousel

--- components/onboardingFlow.vue
+++ components/onboardingFlow.vue
@@ -4,8 +4,8 @@
             <!-- <el-carousel-item v-for="(slide, slideKey) in onboarding" :key="slideKey">
             <h3>{{ slide }}</h3>
             </el-carousel-item> -->
-            <el-carousel-item v-for="item in 10" :key="item">
-            <h3>{{ item }}</h3>
+            <el-carousel-item v-for="(item, itemKey) in onboarding" :key="itemKey">
+            <div v-html= "item.content"></div>
             </el-carousel-item>
         </el-carousel>  
     </div>
@@ -16,23 +16,41 @@ export default {
     setup() {
         
     },
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        onboarding() {return this.$store.getters.getOnboarding},
+    },
+    mounted() {
+        // console.log(onboarding)
+        this.$store.dispatch('getOnboardingContent', true)
+    }
 }
 </script>
 
 <style>
 
-.el-carousel__container {
-  z-index: 2;
-  position: relative;
-  height: 80vh;
-  text-align: center;
+.el-carousel {
+    z-index: 100;
 }
 
+.el-carousel__container {
+    position: relative;
+    height: 90vh;
+    text-align: center;
+    margin: 5vh auto;
+}
+
+.el-carousel__item {
+    padding: 25px;
+}
 .el-carousel__item h3 {
     color: #475669;
     font-size: 18px;
     opacity: 0.75;
-    line-height: 80vh;
     margin: 0;
   }
 
@@ -42,6 +60,10 @@ export default {
 
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
+  }
+
+  .el-carousel__arrow--left, .el-carousel__arrow--right {
+      top: 85vh;
   }
 
 </style>
