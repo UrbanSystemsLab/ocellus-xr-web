7dfc054db3484ac554bfe9c64da7bb252ea84544
Emily Bowe <emilyrbowe@gmail.com>
2021-08-05 15:50:31 -0400

    Created splitLayout component for onboarding

---
+++ components/onboarding/splitLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <h2>What is Heat Risk?</h2>
+            <p>Heat risk is a measure of how likely your health will be impacted by high temperatures or prolonged heat exposure. There are many factors associated with heat risk, including urban and green infrastructure, social vulnerability, and building density. To learn more, explore the concepts below.</p>
+        </div>
+        <div id="wrapper">
+            <map-carousel v-if="1===0" />
+            <img-carousel v-if="1===1" />
+            <accordion-list v-if="1===0"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import accordionList from './accordionList.vue'
+import ImgCarousel from './imgCarousel.vue'
+import MapCarousel from './mapCarousel.vue'
+export default {
+  components: { accordionList, ImgCarousel, MapCarousel },
+    computed: {
+        onboarding() {return this.$store.getters.getOnboarding},
+    },
+}
+</script>
+
+<style scoped>
+    p {
+         word-wrap: normal;
+    }
+</style>
