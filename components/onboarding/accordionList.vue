7d189fa73fe52911e9b9c6d2cd7285376c9ce9aa
Emily Bowe <emilyrbowe@gmail.com>
2021-08-05 15:49:20 -0400

    Created accordion component for splitscreen

---
+++ components/onboarding/accordionList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <el-collapse v-model="activeNames" @change="handleChange">
+            <el-collapse-item title="This" name="">
+                <div>This</div>
+            </el-collapse-item>
+            <el-collapse-item title="That" name="">
+                <div>That</div>
+            </el-collapse-item>
+        </el-collapse>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
