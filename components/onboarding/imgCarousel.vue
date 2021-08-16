290dda3a3355ffad26406c12fc2468bed0b6e015
Emily Bowe <emilyrbowe@gmail.com>
2021-08-16 18:05:19 -0400

    Fixed button placement for carousel

--- components/onboarding/imgCarousel.vue
+++ components/onboarding/imgCarousel.vue
@@ -1,8 +1,8 @@
 <template>
-    <el-carousel height="50vh" direction="horizonal" :autoplay="false" indicator-position="outside">
+    <el-carousel height="50vh" direction="horizonal" :autoplay="false">
         <el-carousel-item v-for="item in 4" :key="item">
             <h3 class="medium">Image {{ item }}</h3>
-            <img src="https://firebasestorage.googleapis.com/v0/b/usl-nyc-portal-prod.appspot.com/o/equity-ar%2Fimages%2FEcological_Resilience-02%20(1).png?alt=media&token=8cc42a3f-62aa-4ade-ac21-eb045b317b23"  width="100%" > 
+            <img src="https://firebasestorage.googleapis.com/v0/b/usl-nyc-portal-prod.appspot.com/o/equity-ar%2Fimages%2FEcological_Resilience-02%20(1).png?alt=media&token=8cc42a3f-62aa-4ade-ac21-eb045b317b23"  width="90%" > 
         </el-carousel-item>
     </el-carousel>
 </template>
